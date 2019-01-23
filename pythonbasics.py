a7ee80d1d4218186b718444700dc4d5e418bedb4
Michael W Chung <michaelwchungstudio@gmail.com>
2019-01-23 17:04:16 -0500

    basic update

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,5 +1,20 @@
 # Python Basics
 
+# Variables
+# - cannot have spaces or symbols other than an underscore (_)
+# - cannot begin with numbers, but can have numbers after the first letter (e.g. var_1)
+breakfast = "steak and eggs"
+
 # Strings
 print("This is a string.")
 
+# Numbers
+# Integers
+integer_example = 12
+# Floats
+float_example = 8.7
+
+# Errors
+# - Python will point to error location with a ^ character
+# - 1. SyntaxError: something is wrong with the way it is written (wrong punctuation, wrong command, missing parenthesis)
+# - 2. NameError: code contains something that is a variable but was never defined
