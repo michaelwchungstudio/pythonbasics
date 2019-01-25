2d810308aa3f2e495066033edf379e19d4f02c25
Michael W Chung <michaelwchungstudio@gmail.com>
2019-01-25 18:02:09 -0500

    modules

---
+++ examplemodule.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print("Hello, " + name)
+
+person1 = {
+    "name": "Michael",
+    "age": 26,
+    "country": "USA"
+}
---
+++ python_modules.py
@@ -0,0 +1,15 @@
+## Python modules
+# equivalent to a code library
+# file containing a set of functions you want to include in your application
+# modules can be created by saving code into a file with the extension .py
+# modules can contain functions but also variables of all types (arrays, dictionaries, objects, etc.)
+
+## Using/importing a module
+import examplemodule
+
+examplemodule.greeting("Michael") # prints 'Hello, Michael'
+print(examplemodule.person1["name"]) # prints 'Michael'
+print(examplemodule.person1["age"]) # prints '26'
+print(examplemodule.person1["country"]) # prints 'USA'
+
+
