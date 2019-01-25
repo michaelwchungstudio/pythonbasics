32b366c52c6053dea54d157a5482844ac7e69929
Michael W Chung <michaelwchungstudio@gmail.com>
2019-01-25 18:46:27 -0500

    modules done

---
+++ examplemodule2.py
@@ -0,0 +1,8 @@
+def print_car(name):
+    print("I drive a " + name)
+
+car1 = {
+    "company": "Toyota",
+    "model": "Prius",
+    "year": 2017
+}
--- python_modules.py
+++ python_modules.py
@@ -13,4 +13,17 @@ print(exmod.person1["name"]) # prints 'Michael'
 print(exmod.person1["age"]) # prints '26'
 print(exmod.person1["country"]) # prints 'USA'
 
+## Importing specifics of a module, use the 'from' keyword
+from examplemodule2 import car1
+print("I drive a " + str(car1["year"]) + " " + car1["company"] + " " + car1["model"] + ".")
+
+## Built-in modules
+# https://docs.python.org/3/py-modindex.html
+import platform
+plat = platform.system()
+print(plat)
+
+## dir() function
+# built-in function to list all the function or variable names in a module
+print(dir(platform))
 
